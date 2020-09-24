93fde4f52b411210bc0e9b34590086e8d43f067b
Griffin Metheny <griffinmetheny@gmail.com>
2020-09-24 17:04:01 -0400

    Add project files.

---
+++ 3A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calls the userName method
+            userName();
+
+        }
+
+        // userName method
+        private static void userName()
+        {
+            // asks user for their name
+            Console.WriteLine("What is your name?");
+            // user input stored as userName
+            string userName = Console.ReadLine();
+            // console prints Hello plus the user input
+            Console.WriteLine("Hello " + userName);
+        }
+    }
+}
+
